941d235a758dbbe239e5f254c3a9ddd7cecea79e
ghani kamil <ghanikamil19@gmail.com>
2025-03-06 13:36:29 +0700

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router'
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -20,25 +20,30 @@ function App() {
         </a>
       </div>
 
-      <div className="card">
-        <p><Link to="/AddProduct">Add Product</Link></p>
+      <div>
+      <p><Link to="/AddProduct">Add Product</Link></p>
         <p><Link to="/AddCategory">Add Category</Link></p>
         <p><Link to="/CartList">Cart</Link></p>
+        <Link to={"/login"}>halaman login</Link>
+      <br/>
+      <Link to={"/register"}>halaman register</Link>
+      </div>
 
 
       <h1>Vite + React</h1>
-      <Link to={"/login"}>halaman login</Link>
-      <br/>
-      <Link to={"/register"}>halaman register</Link>
-
       <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
-          <Link to="/Home">Home</Link>
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-
       </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
 export default App;
